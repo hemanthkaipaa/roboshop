8da9641ae5fdf50b3218742ed5143990b31a27bf
hemanthkaipa <hemanthkaipa@gmail.com>
2025-06-08 12:36:41 +0530

    Creating instances and hosted zones

--- roboshop.sh
+++ roboshop.sh
@@ -6,20 +6,6 @@ ZONE_ID="Z04194951UBY03IZ86RXM"
 DOMAIN_NAME="hkdevops.site"
 INSTANCES=(mongodb mysql redis rabbitmq catalog user cart shipping payment dispatch frontend)
 
-
-for i in "${INSTANCES[@]}" ; do
-    instanceID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].InstanceId" --output text)
-    if [ $i != "frontend" ] ; then
-        IP=$(aws ec2 describe-instances --instance-ids $instanceID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-    else
-        IP=$(aws ec2 describe-instances --instance-ids $instanceID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-    fi
-    echo "$i IP Address : $IP"
-
-    SETUP_HOSTED_ZONE $IP $i
-
-done
-
 SETUP_HOSTED_ZONE(){
     aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch "
     {
@@ -41,3 +27,18 @@ SETUP_HOSTED_ZONE(){
         ]
     }"
 }
+
+
+for i in "${INSTANCES[@]}" ; do
+    instanceID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].InstanceId" --output text)
+    if [ $i != "frontend" ] ; then
+        IP=$(aws ec2 describe-instances --instance-ids $instanceID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $instanceID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$i IP Address : $IP"
+
+    SETUP_HOSTED_ZONE $IP $i
+
+done
+
